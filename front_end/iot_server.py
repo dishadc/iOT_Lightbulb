3637c2de5c64a9c6e58a86803454f4cbc1a17ad0
Dishad <dishad@cs.washignton.edu>
2020-03-20 05:59:56 -0700

    minor changes

--- front_end/iot_server.py
+++ front_end/iot_server.py
@@ -2,8 +2,9 @@ import SimpleHTTPServer
 import SocketServer
 import socket
 import threading
+import sys
 
-PORT = 8000
+PORT = int(sys.argv[1])
 
 # TCP/HTTP Server connection for client application.
 class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
@@ -21,20 +22,47 @@ Handler = MyRequestHandler
 httpd = SocketServer.TCPServer(("", PORT), Handler)
 
 def ClientThread():
-    print "serving at port", PORT
+  print "serving at port", PORT
+  try:
     httpd.serve_forever()
+  except KeyboardInterrupt, SystemExit:
+    pass
+  finally:
+    httpd.shutdown()
+    httpd.server_close()
 
 t1 = threading.Thread(target=ClientThread)
 t1.start()
 
+class LightThread(threading.Thread):
+  def __init__(self, sock, client_sock):
+    threading.Thread.__init__(self)
+    self.sock = sock
+    self.client_sock = client_sock
+
+  def run(self):
+    print "client thread started"
+    try:
+      data = client_sock.recv(1024)
+      print(data)
+    except Exception:
+      return
+
+  
 #Connection to light
+print 'opening socket'
 HOST = ''                # Symbolic name meaning all available interfaces
-PORT = 1234              # Arbitrary non-privileged port
+port = int(sys.argv[2])  # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
-conn, addr = s.accept()
+s.bind((HOST, port))
+s.listen(5)
 while True:
-    data = conn.recv(1024)
-    if not data: break
-    print(data)
+    try:
+      conn, addr = s.accept()
+      lt = LightThread(s, conn)
+      lt.daemon = True
+      lt.start()
+    except KeyboardInterrupt, SystemExit:
+      s.close()
+      sys.exit()
+      print "Shutting down iOT Server"
