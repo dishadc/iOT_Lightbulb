c23df9150273eb73a1934042b6f79f0b47b5b5f7
Peter Davenport <peterdav@uw.edu>
2020-03-19 17:38:04 -0700

    Able to attach to both user (website) and iot device.

--- front_end/iot_server.py
+++ front_end/iot_server.py
@@ -1,8 +1,11 @@
 import SimpleHTTPServer
 import SocketServer
+import socket
+import threading
 
 PORT = 8000
 
+# TCP/HTTP Server connection for client application.
 class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_PUT(self):
         content_length = int(self.headers['Content-Length'])
@@ -17,5 +20,21 @@ Handler = MyRequestHandler
 
 httpd = SocketServer.TCPServer(("", PORT), Handler)
 
-print "serving at port", PORT
-httpd.serve_forever()
+def ClientThread():
+    print "serving at port", PORT
+    httpd.serve_forever()
+
+t1 = threading.Thread(target=ClientThread)
+t1.start()
+
+#Connection to light
+HOST = ''                # Symbolic name meaning all available interfaces
+PORT = 1234              # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+while True:
+    data = conn.recv(1024)
+    if not data: break
+    print(data)
